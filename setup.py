01f8486d8776527500d0cd10b421b81d2952980b
Peter Somers <psomers3@gatech.edu>
2019-10-03 13:54:34 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='tf-bspline',
+    version='1.0.0',
+    description='TensorFlow implementation of b-spline fitter',
+    author='Peter Somers',
+    license='MIT',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    packages=['tfbspline', 'tfbspline.util'],
+    install_requires=['numpy', 'tensorflow']
+)
